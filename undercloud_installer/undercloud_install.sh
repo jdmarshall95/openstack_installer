d9bcc9e5f1a67f58dcabe87262f87a9ca424b3f5
hiraetari <hiraetari@gmail.com>
2020-03-23 13:11:53 +0300

    fixed access rights

--- undercloud_installer/undercloud_install.sh
+++ undercloud_installer/undercloud_install.sh
@@ -1,5 +1,5 @@
 repo_adress=$(sudo locate python2-tripleo-repos.noarch.rpm)
-rpm -ivh $repo_adres
+sudo rpm -ivh $repo_adress
 sudo -E tripleo-repos -b stein current
 sudo yum install python-tripleoclient -y
 ruby_installer_adress=$(sudo locate ruby_install.sh)
@@ -8,5 +8,5 @@ sudo chmod 777 $ruby_installer_adress
 sudo yum -y downgrade leatherman
 
 undercloud_sample_adress=$(sudo locate my_undercloud.conf)
-cp $undercloud_sample_adress undercloud.conf
+sudo cp $undercloud_sample_adress undercloud.conf
 openstack undercloud install
