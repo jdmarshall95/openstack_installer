05dcfc33bc7c7ef9c5578350a389b2647b170e36
hiraetari <hiraetari@gmail.com>
2020-03-23 13:00:30 +0300

    fixed variables

--- undercloud_installer/undercloud_install.sh
+++ undercloud_installer/undercloud_install.sh
@@ -1,12 +1,12 @@
-repo_adress =$(sudo locate python2-tripleo-repos.noarch.rpm)
+repo_adress=$(sudo locate python2-tripleo-repos.noarch.rpm)
 rpm -ivh $repo_adres
 sudo -E tripleo-repos -b stein current
 sudo yum install python-tripleoclient -y
-ruby_installer_adress = $(sudo locate ruby_install.sh)
+ruby_installer_adress=$(sudo locate ruby_install.sh)
 sudo chmod 777 $ruby_installer_adress
 ./$ruby_installer_adress
 sudo yum -y downgrade leatherman
 
-undercloud_sample_adress =$(sudo locate my_undercloud.conf)
+undercloud_sample_adress=$(sudo locate my_undercloud.conf)
 cp $undercloud_sample_adress undercloud.conf
 openstack undercloud install
