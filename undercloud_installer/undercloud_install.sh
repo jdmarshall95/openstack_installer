6050061de98a1026968178f080a6ac0cb724f4e8
hiraetari <hiraetari@gmail.com>
2020-03-23 21:25:26 +0300

    added facter

--- undercloud_installer/undercloud_install.sh
+++ undercloud_installer/undercloud_install.sh
@@ -3,6 +3,7 @@ sudo rpm -ivh $repo_adress
 sudo -E tripleo-repos -b stein current
 sudo yum -y install python-tripleoclient leatherman
 sudo yum -y downgrade leatherman
+facter
 ruby_installer_adress=$(sudo locate ruby_install.sh)
 sudo cp $ruby_installer_adress ruby_install.sh
 sudo chmod 777 ruby_install.sh
