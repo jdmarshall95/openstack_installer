5ad53ebd89a2f8a37ae9dbf82b8f7e1730c1e224
hiraetari <hiraetari@gmail.com>
2020-03-23 13:36:31 +0300

    small fixes

--- undercloud_installer/preparation.sh
+++ undercloud_installer/preparation.sh
@@ -8,11 +8,13 @@ useradd stack
 echo "enter_password_here" | passwd --stdin stack
 echo "stack ALL=(root) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/stack
 chmod 0440 /etc/sudoers.d/stack
-yum -y install mlocate python-requests python3-pip yum-plugin-priorities epel-release vim
+yum -y install mlocate python-requests python3-pip yum-plugin-priorities epel-release vim wget
 
 
 updatedb
 pip3 install requests
 cp undercloud_install.sh /home/stack/undercloud_install.sh
 chmod 777 /home/stack/undercloud_install.sh
+cp my_undercloud.conf /home/stack/undercloud.conf
+
 su - stack  #preparation part ends here
--- undercloud_installer/undercloud_install.sh
+++ undercloud_installer/undercloud_install.sh
@@ -2,11 +2,9 @@ repo_adress=$(sudo locate python2-tripleo-repos.noarch.rpm)
 sudo rpm -ivh $repo_adress
 sudo -E tripleo-repos -b stein current
 sudo yum install python-tripleoclient -y
-ruby_installer_adress=$(sudo locate ruby_install.sh)
-sudo chmod 777 $ruby_installer_adress
-./$ruby_installer_adress
 sudo yum -y downgrade leatherman
-
-undercloud_sample_adress=$(sudo locate my_undercloud.conf)
-sudo cp $undercloud_sample_adress undercloud.conf
+ruby_installer_adress=$(sudo locate ruby_install.sh)
+sudo cp $ruby_installer_adress ruby_install.sh
+sudo chmod 777 ruby_install.sh
+sudo ./ruby_install.sh
 openstack undercloud install
