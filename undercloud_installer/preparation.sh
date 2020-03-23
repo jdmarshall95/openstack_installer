a7439e173ac079e920740e70563c086fc1b0e006
hiraetari <hiraetari@gmail.com>
2020-03-23 14:24:01 +0300

    Added few comments

--- undercloud_installer/preparation.sh
+++ undercloud_installer/preparation.sh
@@ -5,7 +5,7 @@ dhclient -r && dhclient
 echo "192.168.126.1 undercloud.example.com" >> /etc/hosts
 yum update -y
 useradd stack
-echo "enter_password_here" | passwd --stdin stack
+echo "enter_password_here" | passwd --stdin stack #CHANGE PASSWORD HERE
 echo "stack ALL=(root) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/stack
 chmod 0440 /etc/sudoers.d/stack
 yum -y install mlocate python-requests python3-pip yum-plugin-priorities epel-release vim wget
