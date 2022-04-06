eaf8264402708a2c71dde8a167dd70915a46ec8e
Hirae <32942962+jdmarshall95@users.noreply.github.com>
2022-04-06 20:05:07 +0300

    Update undercloud_install.sh

--- undercloud_installer/undercloud_install.sh
+++ undercloud_installer/undercloud_install.sh
@@ -2,10 +2,11 @@ repo_adress=$(sudo locate python2-tripleo-repos.noarch.rpm)
 sudo rpm -ivh $repo_adress
 sudo -E tripleo-repos -b stein current
 sudo yum -y install python-tripleoclient leatherman
-sudo yum -y downgrade leatherman
+
 facter
 ruby_installer_adress=$(sudo locate ruby_install.sh)
 sudo cp $ruby_installer_adress ruby_install.sh
 sudo chmod 777 ruby_install.sh
 sudo ./ruby_install.sh
+sudo yum -y downgrade leatherman
 openstack undercloud install
