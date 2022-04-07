7a816ec4504426f56b42022a573301df9d73105a
Hirae <32942962+jdmarshall95@users.noreply.github.com>
2022-04-07 11:22:08 +0300

    Update undercloud_install.sh

--- undercloud_installer/undercloud_install.sh
+++ undercloud_installer/undercloud_install.sh
@@ -2,7 +2,7 @@ repo_adress=$(sudo locate python2-tripleo-repos.noarch.rpm)
 sudo rpm -ivh $repo_adress
 sudo -E tripleo-repos -b stein current
 sudo yum -y install python-tripleoclient leatherman python2-qpid-proton-0.22.0-1.el7.x86_64
-
+sudo yum -y downgrade leatherman
 facter
 ruby_installer_adress=$(sudo locate ruby_install.sh)
 sudo cp $ruby_installer_adress ruby_install.sh
